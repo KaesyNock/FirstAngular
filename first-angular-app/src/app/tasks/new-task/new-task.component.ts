e84bca1d8b7bc6b8fc039a0783563cf6054ddc3f
Kaesy <kaesynock@gmail.com>
2024-09-10 10:30:27 -0400

    wip

--- first-angular-app/src/app/tasks/new-task/new-task.component.ts
+++ first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -10,12 +11,21 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>()
-  enteredTitle = signal('');
-  // enteredTitle = '';
+  @Output() add = new EventEmitter<NewTaskData>();
+  // enteredTitle = signal('');
+  enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
   onCancel() {
     this.cancel.emit()
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    })
   }
 }
